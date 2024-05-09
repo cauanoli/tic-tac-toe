1b51df46086053e8b626fc3f17821e844592e90d
Cauan <cauandeoliveiracosta@gmail.com>
2024-05-09 08:22:43 -0300

    feat: create winning conditions

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 function createPlayer(symbol) {
     let score = 0;
 
+    function resetScore() {
+        score = 0;
+    }
+
     function increaseScore() {
         score++;
     }
 
     function getScore() {
         return score;
+    }
+
+    function clear() {
+        resetScore();
     }
 
     return {
@@ -18,42 +26,109 @@ function createPlayer(symbol) {
 }
 
 const board = (function () {
-    const maxBoardLength = 9;
+    const boardSize = 3; // size x size board
 
     let full = false;
-    let board = [];
+    let board = [[], [], []];
 
-    // TODO: Implement way to full board
-    function placeValue() {}
+    function placeValue(line, column, symbol) {
+        board[line][column] = symbol;
+    }
 
     function getBoard() {
         return board;
     }
 
-    function isFull() {
-        if (board.length >= maxBoardLength) {
-            full = true;
+    function getRows() {
+        return board;
+    }
+
+    function getColumns() {
+        const columns = [...board.map((_) => [])];
+
+        const addElementToColumns = (row, rowIndex) => {
+            row.forEach((rowElement, rowElementIndex) => {
+                columns[rowElementIndex][rowIndex] = rowElement;
+            });
+        };
+
+        getRows().forEach((row, rowIndex) => {
+            addElementToColumns(row, rowIndex);
+        });
+
+        return columns;
+    }
+
+    function getDiagonals() {
+        const firstDiagonal = [];
+        const secondDiagonal = [];
+
+        for (let i = 0; i <= board.length - 1; i++) {
+            firstDiagonal.push(board[i][board.length - i - 1]);
+            secondDiagonal.push(board[i][i]);
         }
+
+        return [firstDiagonal, secondDiagonal];
+    }
+
+    function isFull() {
+        // Verify if all row has all cells
+        const isRowFull = (row) =>
+            row.length >= boardSize && row.every((cell) => cell !== undefined);
+        full = board.every((row) => isRowFull(row));
 
         return full;
     }
 
     function clear() {
-        board = [];
+        board = [[], [], []];
         full = false;
     }
 
     return {
         placeValue,
         getBoard,
+        getRows,
+        getColumns,
+        getDiagonals,
         isFull,
         clear,
+        boardSize,
     };
 })();
 
-// TODO: Implement winning condition
 const gameController = (function () {
-    function verifyWin() {}
+    const isAllEqual = (array) =>
+        array.every((element) => element === array[0]);
+
+    const isNotEmpty = (array) =>
+        array.find((value) => value !== undefined || value !== null);
+
+    const isFull = (array) =>
+        isNotEmpty(array) && array.length === board.boardSize;
+
+    function verifyWin(board) {
+        const rows = board.getRows();
+        const columns = board.getColumns();
+        const diagonals = board.getDiagonals();
+        console.log({ rows, columns, diagonals });
+
+        const rowWin = rows.find((row) => isFull(row) && isAllEqual(row));
+
+        const columnWin = columns.find(
+            (column) => isFull(column) && isAllEqual(column)
+        );
+        const diagonalWin = diagonals.find(
+            (diagonal) => isFull(diagonal) && isAllEqual(diagonal)
+        );
+
+        const winningConditions = [rowWin, columnWin, diagonalWin];
+
+        return winningConditions.find((condition) => {
+            console.log(condition);
+            return !!condition;
+        });
+    }
 
     return {
         verifyWin,
@@ -63,6 +138,7 @@ const gameController = (function () {
 const game = (function () {
     const playerOne = createPlayer(0);
     const playerTwo = createPlayer(1);
+    let activePlayer = playerOne;
 
     function resetTurn() {
         activePlayer = playerOne;
@@ -76,7 +152,8 @@ const game = (function () {
     function playTurn(line, column) {
         board.placeValue(line, column, activePlayer);
 
-        const win = verifyWin(board.getBoard());
+        const win = gameController.verifyWin(board);
+
         if (win) {
             console.log("win");
             activePlayer.increaseScore();
@@ -86,6 +163,7 @@ const game = (function () {
             resetTurn();
         } else {
             passTurn();
+            console.log("Player Turn:" + activePlayer.symbol);
         }
     }
 
